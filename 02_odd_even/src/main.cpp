3e29668b5dd3e3abda0adb6df1584f6e4bad2aed
Augustin Preda <augustin.preda@siemens.com>
2020-07-28 13:56:09 +0300

    add odd-even project

---
+++ 02_odd_even/src/main.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#include <iostream>
+#include <string>
+
+void printOddOrEven(int number)
+{
+	if (number % 2 == 0)
+	{
+		printf("EVEN\n");
+	}
+	else
+	{
+		printf("ODD\n");
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int number = -13;
+
+	// What is this program expected to do?
+	// - Shows whether an argument is an ODD or EVEN number.
+	// How to launch it?
+	// - Execute the binary and pass a parameter to it?
+	// - E.g. Open CMD in bin/Debug or bin/Release
+	//		  02_odd_even.exe 1		=> Output: ODD
+	//		  02_odd_even.exe 2		=> Output: EVEN
+	//		  02_odd_even.exe 		=> Output: No program arguments found
+	//		
+
+	// Make sure there are some program arguments available.
+	if (argc <= 1)
+	{
+		printf("No program arguments found.\n");
+	}
+	
+	// TODO(Gusti): i don't know why this doesn't work, but someone please FIX it.
+	// --------------- start
+
+	// Get the first argument
+	std::string argumentAsString = argv[1];
+
+	//int number = argv[1];
+	//int number = atoi(argumentValues[1]);
+	std::cout << argumentAsString << std::endl;
+	//int number = std::stoi(
+	// --------------- stop
+
+	printOddOrEven(number);
+
+	return 0;
+}
