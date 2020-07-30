659430e2c5990307b1c0047d1c1650cea17a0765
Augustin Preda <augustin.preda@siemens.com>
2020-07-30 13:03:49 +0300

    Add armstrong and books projects

---
+++ 03_armstrong/src/armstrong_main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+bool isArmstrongNumber(int number)
+{
+	// TODO: implement some functionality to see if this number is an armstrong number
+
+	return false;
+}
+
+void printIsArmstrong(int number)
+{
+	if (isArmstrongNumber(number))
+	{
+		std::cout << "Armstrong" << std::endl;
+	}
+	else
+	{
+		std::cout << "NOT Armstrong" << std::endl;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	// What is this program expected to do?
+	// - Shows whether an argument is an armstrong number.
+	// (what?)
+	// -	An Armstrong number is a number that is equal to the sum of cubes of its digits.
+	//		For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
+	//		Let's try to understand why 153 is an Armstrong number:
+	//			1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+	//
+	// How to launch it?
+	// - Execute the binary and pass a parameter to it?
+	// - E.g. Open CMD in bin/Debug or bin/Release
+	//		  03_armstrong.exe 1		=> Output: Armstrong
+	//		  03_armstrong.exe 2		=> Output: Armstrong
+	//		  03_armstrong.exe 			=> Output: No program arguments found.
+	//		  03_armstrong.exe ABC		=> Undefined output (do whatever).
+	//		  03_armstrong.exe 153		=> Output: Armstrong
+	//		  03_armstrong.exe 154		=> Output: NOT Armstrong
+	//
+
+	// Make sure there are some program arguments available.
+	if (argc <= 1)
+	{
+		std::cout << "No program arguments found." << std::endl;
+		return 1;
+	}
+
+	int readNumber = 0;
+	// Get the first argument
+	std::string argumentAsString = argv[1];
+	
+	// TODO: read number / cast to integer
+
+	printIsArmstrong(readNumber);
+	return 0;
+}
---
+++ 04_display_book/src/main_books.cpp
@@ -0,0 +1,96 @@
+﻿#include <string.h>
+#include <iostream>
+
+// Define the maximum length for the name to use.
+// Specify the length of characters in the content (Excluding the terminator).
+#define MAX_NAME_LEN 10
+#define MAX_TITLE_LEN 50
+
+struct Author
+{
+	char name[MAX_NAME_LEN + 1];
+
+	void setName(char nameToSet[MAX_NAME_LEN])
+	{
+		strncpy(name, nameToSet, MAX_NAME_LEN);
+		// overwrite the last character
+		name[MAX_NAME_LEN] = 0;
+	}
+
+	void print()
+	{
+		std::cout << name << std::endl;
+	}
+};
+
+struct Book
+{
+	int id;
+	char title[50];
+
+	int numAuthors;
+	Author authors[5];
+
+	void addAuthor(Author author)
+	{
+		// TODO: add an author to the container authors array.
+		authors[numAuthors] = author;
+		numAuthors++;
+	}
+
+	void print()
+	{
+		std::cout << "Book #" << id << std::endl;
+		std::cout << "------" << std::endl;
+		std::cout << this->title << std::endl;
+
+		// TODO: add all authors
+
+	}
+};
+
+void setBookName(Book& book, std::string name)
+{
+	strncpy(book.title, name.c_str(), MAX_TITLE_LEN);
+	// overwrite the last character
+	book.title[MAX_TITLE_LEN] = 0;
+}
+
+int main()
+{
+	Book book1;
+	Book book2;
+	Book book3;
+	Author author;
+
+	// Load the data into books
+	book1.id = 1;
+	book1.numAuthors = 0;
+	setBookName(book1, u8"The origin of truth (nu există, nu o căutați)");
+	author.setName("Gusti");
+	book1.addAuthor(author);
+
+	book2.id = 2;
+	book2.numAuthors = 0;
+	setBookName(book2, "Arhanghelul Raul");
+	author.setName("Ovidiu Eftimie");
+	book2.addAuthor(author);
+
+	book3.id = 3;
+	book3.numAuthors = 0;
+	setBookName(book3, "Factfulness");
+	author.setName("Hans Rosling");
+	book3.addAuthor(author);
+
+	author.setName("Ola Rosling");
+	book3.addAuthor(author);
+	
+	author.setName("Anna Rosling Ronnlund");
+	book3.addAuthor(author);
+
+	// Display the books
+	book1.print();
+	book2.print();
+	book3.print();
+	return 0;
+}
